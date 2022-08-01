51da2d2df6c6e564c1879860e7f0f8df1c68a7da
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2022-08-01 11:48:40 -0400

    Call only geos-gtfv3 interface, and not fv_dynamics

--- FV_StateMod.F90
+++ FV_StateMod.F90
@@ -1162,7 +1162,7 @@ subroutine FV_Run (STATE, CLOCK, GC, RC)
   logical :: NWAT_TEST
 
   type(ESMF_VM) :: vm
-  integer :: comm, rank, nranks, mpierr
+  integer :: comm, rank, mpierr
   logical :: halting_mode(5)
   real :: start, finish
 
@@ -1722,8 +1722,6 @@ subroutine FV_Run (STATE, CLOCK, GC, RC)
     if (.not. FV_OFF) then
     call set_domain(FV_Atm(1)%domain)  ! needed for diagnostic output done in fv_dynamics
 
-    call MPI_Comm_size(MPI_COMM_WORLD, nranks, mpierr)
-
     ! A workaround to the issue of SIGFPE abort during importing of numpy, is to
     ! disable trapping of floating point exceptions temporarily, call the interface
     ! to the Python function and resume trapping
@@ -1751,19 +1749,35 @@ subroutine FV_Run (STATE, CLOCK, GC, RC)
     call ieee_set_halting_mode(ieee_all, halting_mode)
     print *, rank, ', geos_gtfv3_interface_f: time taken = ', finish - start, 's'
 
-    call cpu_time(start)
-    call fv_dynamics(FV_Atm(1)%npx, FV_Atm(1)%npy, FV_Atm(1)%npz, FV_Atm(1)%ncnst, FV_Atm(1)%ng,   &
-                     myDT, FV_Atm(1)%flagstruct%consv_te, FV_Atm(1)%flagstruct%fill, FV_Atm(1)%flagstruct%reproduce_sum, kappa,   &
-                     cp, zvir, FV_Atm(1)%ptop, FV_Atm(1)%ks, FV_Atm(1)%flagstruct%ncnst, FV_Atm(1)%flagstruct%n_split, FV_Atm(1)%flagstruct%q_split, &
-                     FV_Atm(1)%u, FV_Atm(1)%v, FV_Atm(1)%w, FV_Atm(1)%delz,       &
-                     FV_Atm(1)%flagstruct%hydrostatic, FV_Atm(1)%pt, FV_Atm(1)%delp, FV_Atm(1)%q, FV_Atm(1)%ps,       &
-                     FV_Atm(1)%pe, FV_Atm(1)%pk, FV_Atm(1)%peln, FV_Atm(1)%pkz,                         &
-                     FV_Atm(1)%phis, FV_Atm(1)%q_con, FV_Atm(1)%omga, FV_Atm(1)%ua, FV_Atm(1)%va, FV_Atm(1)%uc, FV_Atm(1)%vc,  &
-                     FV_Atm(1)%ak, FV_Atm(1)%bk, FV_Atm(1)%mfx, FV_Atm(1)%mfy, FV_Atm(1)%cx, FV_Atm(1)%cy,    &
-                     FV_Atm(1)%ze0, FV_Atm(1)%flagstruct%hybrid_z, FV_Atm(1)%gridstruct, FV_Atm(1)%flagstruct, &
-                     FV_Atm(1)%neststruct, FV_Atm(1)%idiag, FV_Atm(1)%bd, FV_Atm(1)%parent_grid, FV_Atm(1)%domain, FV_Atm(1)%diss_est, time_total)
-    call cpu_time(finish)
-    print *, rank, ', fv_dynamics: time taken = ', finish - start, 's'
+    ! call cpu_time(start)
+    ! call fv_dynamics(FV_Atm(1)%npx, FV_Atm(1)%npy, FV_Atm(1)%npz, FV_Atm(1)%ncnst, FV_Atm(1)%ng,   &
+    !                  myDT, FV_Atm(1)%flagstruct%consv_te, FV_Atm(1)%flagstruct%fill, FV_Atm(1)%flagstruct%reproduce_sum, kappa,   &
+    !                  cp, zvir, FV_Atm(1)%ptop, FV_Atm(1)%ks, FV_Atm(1)%flagstruct%ncnst, FV_Atm(1)%flagstruct%n_split, FV_Atm(1)%flagstruct%q_split, &
+    !                  FV_Atm(1)%u, FV_Atm(1)%v, FV_Atm(1)%w, FV_Atm(1)%delz,       &
+    !                  FV_Atm(1)%flagstruct%hydrostatic, FV_Atm(1)%pt, FV_Atm(1)%delp, FV_Atm(1)%q, FV_Atm(1)%ps,       &
+    !                  FV_Atm(1)%pe, FV_Atm(1)%pk, FV_Atm(1)%peln, FV_Atm(1)%pkz,                         &
+    !                  FV_Atm(1)%phis, FV_Atm(1)%q_con, FV_Atm(1)%omga, FV_Atm(1)%ua, FV_Atm(1)%va, FV_Atm(1)%uc, FV_Atm(1)%vc,  &
+    !                  FV_Atm(1)%ak, FV_Atm(1)%bk, FV_Atm(1)%mfx, FV_Atm(1)%mfy, FV_Atm(1)%cx, FV_Atm(1)%cy,    &
+    !                  FV_Atm(1)%ze0, FV_Atm(1)%flagstruct%hybrid_z, FV_Atm(1)%gridstruct, FV_Atm(1)%flagstruct, &
+    !                  FV_Atm(1)%neststruct, FV_Atm(1)%idiag, FV_Atm(1)%bd, FV_Atm(1)%parent_grid, FV_Atm(1)%domain, FV_Atm(1)%diss_est, time_total)
+    ! call cpu_time(finish)
+    ! print *, rank, ', fv_dynamics: time taken = ', finish - start, 's'
+
+    ! block
+    !   character(len=256) :: out_file
+    !   integer :: unit
+    !   write(out_file, '(a8, i0.2, a4)') 'fv3-out.', rank, '.bin'
+    !   open(newunit=unit, file=out_file, form='unformatted', status='new')
+    !   write(unit) isd, ied, jsd, jed, FV_Atm(1)%bd%is, FV_Atm(1)%bd%ie, FV_Atm(1)%bd%js, FV_Atm(1)%bd%je, FV_Atm(1)%npz
+    !   write(unit) FV_Atm(1)%u, FV_Atm(1)%v, FV_Atm(1)%w, FV_Atm(1)%delz
+    !   write(unit) FV_Atm(1)%pt, FV_Atm(1)%delp, FV_Atm(1)%q(:,:,:,1:7)
+    !   write(unit) FV_Atm(1)%ps, FV_Atm(1)%pe, FV_Atm(1)%pk, FV_Atm(1)%peln, FV_Atm(1)%pkz
+    !   write(unit) FV_Atm(1)%phis, FV_Atm(1)%q_con, FV_Atm(1)%omga
+    !   write(unit) FV_Atm(1)%ua, FV_Atm(1)%va, FV_Atm(1)%uc, FV_Atm(1)%vc
+    !   write(unit) FV_Atm(1)%mfx, FV_Atm(1)%mfy, FV_Atm(1)%cx, FV_Atm(1)%cy
+    !   write(unit) FV_Atm(1)%diss_est
+    !   close(unit)
+    ! end block    
 
     if ( FV_Atm(1)%flagstruct%fv_sg_adj > 0 ) then
          allocate ( u_dt(isd:ied,jsd:jed,npz) )
