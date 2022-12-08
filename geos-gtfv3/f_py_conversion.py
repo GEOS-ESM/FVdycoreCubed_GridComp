b8ccb0ec109d74bd8db6a0d8e14f4262a8bfd006
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2022-12-08 11:46:01 -0500

    Upgrade gtfv3 (pace/fv3core) to commit df94e78f (12/08/22)
    - use dace:gpu backend
    - use GeosDycoreWrapper interface
      - geos_gtfv3_initialize/run are not needed any more
      - splitting of 'q' is now done in the wrapper
      - axes 'k' and 'j' of variables 'pe' and 'peln' need to be swapped outside the wrapper

--- geos-gtfv3/f_py_conversion.py
+++ geos-gtfv3/f_py_conversion.py
@@ -40,8 +40,11 @@ def fortran_state_to_numpy(
     Output: dict where dict[key] is a NumPy array plus consv_te etc.
     '''
 
-    # 'q' requires special handling
+    # q/pe/peln require special handling
+    # pe/peln need to be have their axes swapped - (i, k, j) -> (i, j, k)
     q = fort_to_numpy(q_ptr, (ied-isd+1, jed-jsd+1, npz, nq_tot))
+    pe = np.swapaxes(fort_to_numpy(pe_ptr, (ie+1-(is_-1)+1, npz+1, je+1-(js-1)+1)), 1, 2)
+    peln = np.swapaxes(fort_to_numpy(peln_ptr, (ie-is_+1, npz+1, je-js+1)), 1, 2)
 
     numpy_state = {
         'do_adiabatic_init': True, # REQUIRED (TODO???)
@@ -59,22 +62,13 @@ def fortran_state_to_numpy(
 
         'pt': fort_to_numpy(pt_ptr, (ied-isd+1, jed-jsd+1, npz)),
         'delp': fort_to_numpy(delp_ptr, (ied-isd+1, jed-jsd+1, npz)),
-        'qvapor': q[:,:,:,0],
-        'qliquid': q[:,:,:,1],
-        'qice': q[:,:,:,2],
-        'qrain': q[:,:,:,3],
-        'qsnow': q[:,:,:,4],
-        'qgraupel': q[:,:,:,5],
-        'qcld': q[:,:,:,6],
-        # GEOS does not provide qo3mr and qsgs_tke - setting those to zero
-        'qo3mr': np.zeros(q[:,:,:,0].shape),
-        'qsgs_tke': np.zeros(q[:,:,:,0].shape),
+        'q': q,
 
         'ps': fort_to_numpy(ps_ptr, (ied-isd+1, jed-jsd+1)),
-        'pe': fort_to_numpy(pe_ptr, (ie+1-(is_-1)+1, npz+1, je+1-(js-1)+1)),
+        'pe': pe,
         'pk': fort_to_numpy(pk_ptr, (ie-is_+1, je-js+1, npz+1)),
         'pkz': fort_to_numpy(pkz_ptr, (ie-is_+1, je-js+1, npz)),
-        'peln': fort_to_numpy(peln_ptr, (ie-is_+1, npz+1, je-js+1)),
+        'peln': peln,
 
         'phis': fort_to_numpy(phis_ptr, (ied-isd+1, jed-jsd+1)),
         'q_con': fort_to_numpy(q_con_ptr, (ied-isd+1, jed-jsd+1, npz)),
@@ -95,8 +89,8 @@ def fortran_state_to_numpy(
 
 
 
-def numpy_output_data_to_fortran(
-        output_data,
+def numpy_state_to_fortran(
+        numpy_state,
         u_ptr, v_ptr, w_ptr, delz_ptr,
         pt_ptr, delp_ptr, q_ptr,
         ps_ptr, pe_ptr, pk_ptr, peln_ptr, pkz_ptr,
@@ -105,67 +99,69 @@ def numpy_output_data_to_fortran(
         mfxd_ptr, mfyd_ptr, cxd_ptr, cyd_ptr, diss_estd_ptr):
 
     # NumPy -> Fortran
-    u_out_f = output_data['u'].astype(np.float32).flatten(order='F')
+    u_out_f = numpy_state['u'].astype(np.float32).flatten(order='F')
     ffi.memmove(u_ptr, u_out_f, 4*u_out_f.size)
-    v_out_f = output_data['v'].astype(np.float32).flatten(order='F')
+    v_out_f = numpy_state['v'].astype(np.float32).flatten(order='F')
     ffi.memmove(v_ptr, v_out_f, 4*v_out_f.size)
-    w_out_f = output_data['w'].astype(np.float32).flatten(order='F')
+    w_out_f = numpy_state['w'].astype(np.float32).flatten(order='F')
     ffi.memmove(w_ptr, w_out_f, 4*w_out_f.size)
-    delz_out_f = output_data['delz'].astype(np.float32).flatten(order='F')
+    delz_out_f = numpy_state['delz'].astype(np.float32).flatten(order='F')
     ffi.memmove(delz_ptr, delz_out_f, 4*delz_out_f.size)
 
-    pt_out_f = output_data['pt'].astype(np.float32).flatten(order='F')
+    pt_out_f = numpy_state['pt'].astype(np.float32).flatten(order='F')
     ffi.memmove(pt_ptr, pt_out_f, 4*pt_out_f.size)
-    delp_out_f = output_data['delp'].astype(np.float32).flatten(order='F')
+    delp_out_f = numpy_state['delp'].astype(np.float32).flatten(order='F')
     ffi.memmove(delp_ptr, delp_out_f, 4*delp_out_f.size)
     # q needs special handling
-    q = np.empty(list(output_data['qvapor'].shape)+[9])
-    q[:,:,:,0] = output_data['qvapor']
-    q[:,:,:,1] = output_data['qliquid']
-    q[:,:,:,2] = output_data['qice']
-    q[:,:,:,3] = output_data['qrain']
-    q[:,:,:,4] = output_data['qsnow']
-    q[:,:,:,5] = output_data['qgraupel']
-    q[:,:,:,6] = output_data['qcld']
-    # q[:,:,:,7] = output_data['qo3mr']
-    # q[:,:,:,8] = output_data['qsgs_tke']
+    q = np.empty(list(numpy_state['qvapor'].shape)+[9])
+    q[:,:,:,0] = numpy_state['qvapor']
+    q[:,:,:,1] = numpy_state['qliquid']
+    q[:,:,:,2] = numpy_state['qice']
+    q[:,:,:,3] = numpy_state['qrain']
+    q[:,:,:,4] = numpy_state['qsnow']
+    q[:,:,:,5] = numpy_state['qgraupel']
+    q[:,:,:,6] = numpy_state['qcld']
+    # q[:,:,:,7] = numpy_state['qo3mr']
+    # q[:,:,:,8] = numpy_state['qsgs_tke']
     q_out_f = q.astype(np.float32).flatten(order='F')
     ffi.memmove(q_ptr, q_out_f, 4*q_out_f.size)
 
-    ps_out_f = output_data['ps'].astype(np.float32).flatten(order='F')
+    ps_out_f = numpy_state['ps'].astype(np.float32).flatten(order='F')
     ffi.memmove(ps_ptr, ps_out_f, 4*ps_out_f.size)
-    pe_out_f = output_data['pe'].astype(np.float32).flatten(order='F')
+    pe_out_tmp = np.swapaxes(numpy_state['pe'], 1, 2) # (i, j, k) -> (i, k, j)
+    pe_out_f = pe_out_tmp.astype(np.float32).flatten(order='F')
     ffi.memmove(pe_ptr, pe_out_f, 4*pe_out_f.size)
-    pk_out_f = output_data['pk'].astype(np.float32).flatten(order='F')
+    pk_out_f = numpy_state['pk'].astype(np.float32).flatten(order='F')
     ffi.memmove(pk_ptr, pk_out_f, 4*pk_out_f.size)
-    peln_out_f = output_data['peln'].astype(np.float32).flatten(order='F')
+    peln_out_tmp = np.swapaxes(numpy_state['peln'], 1, 2) # (i, j, k) -> (i, k, j)
+    peln_out_f = peln_out_tmp.astype(np.float32).flatten(order='F')
     ffi.memmove(peln_ptr, peln_out_f, 4*peln_out_f.size)
-    pkz_out_f = output_data['pkz'].astype(np.float32).flatten(order='F')
+    pkz_out_f = numpy_state['pkz'].astype(np.float32).flatten(order='F')
     ffi.memmove(pkz_ptr, pkz_out_f, 4*pkz_out_f.size)
 
-    phis_out_f = output_data['phis'].astype(np.float32).flatten(order='F')
+    phis_out_f = numpy_state['phis'].astype(np.float32).flatten(order='F')
     ffi.memmove(phis_ptr, phis_out_f, 4*phis_out_f.size)
-    q_con_out_f = output_data['q_con'].astype(np.float32).flatten(order='F')
+    q_con_out_f = numpy_state['q_con'].astype(np.float32).flatten(order='F')
     ffi.memmove(q_con_ptr, q_con_out_f, 4*q_con_out_f.size)
-    omga_out_f = output_data['omga'].astype(np.float32).flatten(order='F')
+    omga_out_f = numpy_state['omga'].astype(np.float32).flatten(order='F')
     ffi.memmove(omga_ptr, omga_out_f, 4*omga_out_f.size)
 
-    ua_out_f = output_data['ua'].astype(np.float32).flatten(order='F')
+    ua_out_f = numpy_state['ua'].astype(np.float32).flatten(order='F')
     ffi.memmove(ua_ptr, ua_out_f, 4*ua_out_f.size)
-    va_out_f = output_data['va'].astype(np.float32).flatten(order='F')
+    va_out_f = numpy_state['va'].astype(np.float32).flatten(order='F')
     ffi.memmove(va_ptr, va_out_f, 4*va_out_f.size)
-    uc_out_f = output_data['uc'].astype(np.float32).flatten(order='F')
+    uc_out_f = numpy_state['uc'].astype(np.float32).flatten(order='F')
     ffi.memmove(uc_ptr, uc_out_f, 4*uc_out_f.size)
-    vc_out_f = output_data['vc'].astype(np.float32).flatten(order='F')
+    vc_out_f = numpy_state['vc'].astype(np.float32).flatten(order='F')
     ffi.memmove(vc_ptr, vc_out_f, 4*vc_out_f.size)
 
-    mfxd_out_f = output_data['mfxd'].astype(np.float32).flatten(order='F')
+    mfxd_out_f = numpy_state['mfxd'].astype(np.float32).flatten(order='F')
     ffi.memmove(mfxd_ptr, mfxd_out_f, 4*mfxd_out_f.size)
-    mfyd_out_f = output_data['mfyd'].astype(np.float32).flatten(order='F')
+    mfyd_out_f = numpy_state['mfyd'].astype(np.float32).flatten(order='F')
     ffi.memmove(mfyd_ptr, mfyd_out_f, 4*mfyd_out_f.size)
-    cxd_out_f = output_data['cxd'].astype(np.float32).flatten(order='F')
+    cxd_out_f = numpy_state['cxd'].astype(np.float32).flatten(order='F')
     ffi.memmove(cxd_ptr, cxd_out_f, 4*cxd_out_f.size)
-    cyd_out_f = output_data['cyd'].astype(np.float32).flatten(order='F')
+    cyd_out_f = numpy_state['cyd'].astype(np.float32).flatten(order='F')
     ffi.memmove(cyd_ptr, cyd_out_f, 4*cyd_out_f.size)
-    diss_estd_out_f = output_data['diss_estd'].astype(np.float32).flatten(order='F')
+    diss_estd_out_f = numpy_state['diss_estd'].astype(np.float32).flatten(order='F')
     ffi.memmove(diss_estd_ptr, diss_estd_out_f, 4*diss_estd_out_f.size)
--- geos-gtfv3/geos_gtfv3.py
+++ geos-gtfv3/geos_gtfv3.py
@@ -1,10 +1,16 @@
+import f90nml
 from datetime import datetime
+
 from f_py_conversion import fortran_state_to_numpy
-from f_py_conversion import numpy_output_data_to_fortran
-from geos_gtfv3_initialize import setup_namelist
-from geos_gtfv3_initialize import setup_dycore
-from geos_gtfv3_run import geos_gtfv3_run
-from geos_gtfv3_debug import write_sum_of_vars
+from f_py_conversion import numpy_state_to_fortran
+
+from pace.fv3core.initialization.geos_wrapper import GeosDycoreWrapper
+
+from geos_gtfv3_debug import write_shape_or_value
+
+#-GLOBAL-variables-start-
+GTFV3_DYCORE = None
+#-GLOBAL-variables-end-
 
 def geos_gtfv3(
         comm,
@@ -19,7 +25,7 @@ def geos_gtfv3(
         ak, bk,
         mfx, mfy, cx, cy, diss_est):
 
-    BACKEND = 'gtcuda'
+    BACKEND = 'dace:gpu'
 
     rank = comm.Get_rank()
 
@@ -27,18 +33,7 @@ def geos_gtfv3(
         print('P:', datetime.now().isoformat(timespec='milliseconds'),
               '--in top level geos-gtfv3, backend:', BACKEND, flush=True)
 
-    # Initialize namelist
-    # This function gets exercised only the first timestep
-    dycore_config = setup_namelist(npx, npy, npz, ntiles, [layout_1, layout_2])
-    assert dycore_config is not None
-
-    if rank == 0:
-        print('P:', datetime.now().isoformat(timespec='milliseconds'),
-              '--setup namelist completed', flush=True)
-        print(dycore_config)
-
     # Convert Fortran arrays to NumPy
-    # This function gets exercised at every timestep
     state_in = fortran_state_to_numpy(
         npx, npy, npz,
         is_, ie, js, je, isd, ied, jsd, jed,
@@ -52,35 +47,35 @@ def geos_gtfv3(
         ua, va, uc, vc,
         mfx, mfy, cx, cy, diss_est)
     # write_sum_of_vars(comm, state_in)
+    # write_shape_or_value(comm, state_in)
 
     if rank == 0:
         print('P:', datetime.now().isoformat(timespec='milliseconds'),
               '--created input data', flush=True)
 
-    # Initialize dycore
-    # This function gets exercised only the first timestep
-    driver_object, dycore = setup_dycore(
-        BACKEND, comm,
-        npx, npy, npz,
-        is_, ie, js, je, isd, ied, jsd, jed,
-        state_in)
-    assert dycore is not None, 'dycore is None'
+    # Setup dycore object
+    # This block gets executed only at the first time step
+    global GTFV3_DYCORE
+    if GTFV3_DYCORE is None:
+        namelist = f90nml.read('input.nml')
+        GTFV3_DYCORE = GeosDycoreWrapper(namelist, bdt, comm, BACKEND)
+    assert GTFV3_DYCORE is not None, 'GTFV3_DYCORE is None'
 
     if rank == 0:
         print('P:', datetime.now().isoformat(timespec='milliseconds'),
               '--initialized dycore', flush=True)
 
     # Run gtFV3
-    state_out = geos_gtfv3_run(
-        comm,
-        dycore_config,
-        driver_object,
-        dycore,
-        state_in)
-    # write_sum_of_vars(comm, state_out)
+    state_out = GTFV3_DYCORE(
+        state_in['u'], state_in['v'], state_in['w'], state_in['delz'],
+        state_in['pt'], state_in['delp'], state_in['q'],
+        state_in['ps'], state_in['pe'], state_in['pk'], state_in['peln'], state_in['pkz'],
+        state_in['phis'], state_in['q_con'], state_in['omga'],
+        state_in['ua'], state_in['va'], state_in['uc'], state_in['vc'],
+        state_in['mfxd'], state_in['mfyd'], state_in['cxd'], state_in['cyd'], state_in['diss_estd'])
 
     # Convert NumPy arrays back to Fortran
-    numpy_output_data_to_fortran(
+    numpy_state_to_fortran(
         state_out,
         u, v, w, delz,
         pt, delp, q,
