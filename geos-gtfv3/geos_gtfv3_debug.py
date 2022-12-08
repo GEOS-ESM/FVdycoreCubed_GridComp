bf8426414baee247b597015b21e7ddd27e79fec2
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2022-12-08 11:42:28 -0500

    Added function to print shape (arrays) or value (scalars) of incoming data dict

--- geos-gtfv3/geos_gtfv3_debug.py
+++ geos-gtfv3/geos_gtfv3_debug.py
@@ -14,3 +14,19 @@ def write_sum_of_vars(comm, data, var_list=None):
             for varname in var_list:
                 print('P:', varname, ':', data[varname].shape, np.sum(data[varname]), flush=True)
         comm.Barrier()
+
+def write_shape_or_value(comm, data):
+
+    rank = comm.Get_rank()
+    nranks = comm.Get_size()
+
+    for i in range(nranks):
+        if i == rank:
+            print()
+            print('P: rank:', rank, flush=True)
+            for varname in data:
+                try:
+                    print(varname, data[varname].shape)
+                except:
+                    print(varname, data[varname])
+        comm.Barrier()
