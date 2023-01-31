c5a0ddbbc75dac3e783a68ad327cc996d276defc
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2023-01-30 21:46:27 -0500

    Fix run path

--- geos-gtfv3/driver/setenv/run.sh
+++ geos-gtfv3/driver/setenv/run.sh
@@ -1,7 +1,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # PACE directories
-PACEDIR=/discover/nobackup/pchakrab/code/ext/ai2/pace/6d5bb26
+PACEDIR=../../../@gtFV3
 export PYTHONPATH=${PYTHONPATH}:${PACEDIR}/dsl
 export PYTHONPATH=${PYTHONPATH}:${PACEDIR}/util
 export PYTHONPATH=${PYTHONPATH}:${PACEDIR}/driver
@@ -10,4 +10,4 @@ export PYTHONPATH=${PYTHONPATH}:${PACEDIR}/fv3core
 export PYTHONPATH=${PYTHONPATH}:${PACEDIR}/stencils
 
 # Directory containing geos-gtfv3.py
-export PYTHONPATH=${PYTHONPATH}:${SCRIPT_DIR}/../../..
+export PYTHONPATH=${PYTHONPATH}:${SCRIPT_DIR}/../../../geos-gtfv3
