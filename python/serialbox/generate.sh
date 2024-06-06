4e7e5a7f03f09e15ed48afdf824245399de0314a
Florian Deconinck <deconinck.florian@gmail.com>
2024-06-06 06:17:12 -0400

    Update to the generate.sh script

--- python/serialbox/generate.sh
+++ python/serialbox/generate.sh
@@ -2,8 +2,14 @@
 
 ml SMTStack/2024.04.00
 
-FV_COMP=../../
+FV_COMP=/home/fgdeconi/work/git/fp/geos/src/Components/@GEOSgcm_GridComp/GEOSagcm_GridComp/GEOSsuperdyn_GridComp/@FVdycoreCubed_GridComp
+
+# find ../../ -iname *F90.SER
 
 FILE=$FV_COMP/FV_StateMod.F90
+python3 $SERIALBOX_ROOT/python/pp_ser/pp_ser.py $FILE.SER > $FILE
+tail -n +2 $FILE > $FILE.swp && mv $FILE.swp $FILE
+
+FILE=$FV_COMP/@fvdycore/model/dyn_core.F90
 python3 $SERIALBOX_ROOT/python/pp_ser/pp_ser.py $FILE.SER > $FILE
 tail -n +2 $FILE > $FILE.swp && mv $FILE.swp $FILE
