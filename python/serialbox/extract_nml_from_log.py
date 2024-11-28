711ba05405d41da4e4ab6e7e48044ca1fb9392d4
Florian Deconinck <deconinck.florian@gmail.com>
2024-11-28 16:35:32 -0500

    Fix extractor and add key New automatic .SER gen via python

--- python/serialbox/extract_nml_from_log.py
+++ python/serialbox/extract_nml_from_log.py
@@ -1,26 +1,29 @@
 import sys
 
-def main(log_file:str):
+start_key = "$SERIALBOX START FV NML"
+stop_key = "$SERIALBOX STOP FV NML"
+
+
+def main(log_file: str):
     with open(log_file, "r") as f:
         lines = f.readlines()
 
     line_start = -1
     for i, line in enumerate(lines):
-        if "$SERIALBOX START FV NML" in line:
+        if start_key in line:
             line_start = i
 
-    
     with open("input.nml", "w") as nml_f:
         nml_f.write("&fv_core_nml\n")
 
         for line_index in range(line_start, len(lines)):
             line = lines[line_index]
-            if "$SERIALBOX STOP FV NML" in line:
+            if stop_key in line:
                 break
             line = line.replace("FV3", "").replace("\n", "")
             elts = line.split(":")
             if len(elts) == 2:
-                if elts[1] != '':
+                if elts[1] != "":
                     value = elts[1].strip()
                     if value == "F":
                         value = ".false."
@@ -30,6 +33,7 @@ def main(log_file:str):
                     nml_f.write(f" {key} = {value}\n")
         nml_f.write(" layout = 1,1\n")
         nml_f.write("/\n")
+
 
 if __name__ == "__main__":
     main(sys.argv[1])
