8bd57d7ea9ebcfc5799c3810b2147c1f1e0fe07e
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2025-12-11 12:07:16 -0500

    Removed instrumentation of individual methods in favor of MAPL3 instrumentation

--- DynCore_GridCompMod.F90
+++ DynCore_GridCompMod.F90
@@ -343,12 +343,8 @@ contains
 
       type(DynState), pointer :: self
       character(len=ESMF_MAXSTR) :: myTracer
-      class(logger_t), pointer :: logger
       integer :: ilev, itracer, status
       logical :: FV3_STANDALONE
-
-      call MAPL_GridCompGet(gc, logger=logger, _RC)
-      call logger%info("SetServices:: starting...")
 
       ! Wrap gridcomp's private state and store it in gc
       _SET_NAMED_PRIVATE_STATE(gc, DynState, PRIVATE_STATE)
@@ -439,7 +435,6 @@ contains
       call MAPL_GridCompGetResource(gc, "DEBUG_ADV", DEBUG_ADV, default=.false., _RC)
       call MAPL_GridCompGetResource(gc, "DEBUG_TQ_ERRORS", DEBUG_TQ_ERRORS, default=.false., _RC)
 
-      call logger%info("SetServices:: ...complete")
       _RETURN(_SUCCESS)
 
    end subroutine SetServices
@@ -476,11 +471,6 @@ contains
       logical :: ColdRestart, FV3_STANDALONE
       integer :: ifirst, ilast, jfirst, jlast, km
       integer :: i, numTracers, status
-
-      class(logger_t), pointer :: logger
-
-      call MAPL_GridCompGet(gc, logger=logger, _RC)
-      call logger%info("Initialize:: starting...")
 
       ! ! Start the timers
       ! call MAPL_TimerOn(MAPL, "TOTAL")
@@ -607,7 +597,6 @@ contains
       ! call MAPL_TimerOff(MAPL,"INITIALIZE")
       ! call MAPL_TimerOff(MAPL,"TOTAL")
 
-      call logger%info("Initialize:: ...complete")
       _RETURN(_SUCCESS)
    end subroutine Initialize
 
@@ -824,7 +813,6 @@ contains
       class(logger_t), pointer :: logger
 
       call MAPL_GridCompGet(gc, grid=esmfgrid, hconfig=hconfig, logger=logger, _RC)
-      call logger%info("Run:: starting...")
       call ESMF_GridValidate(esmfgrid, _RC)
 
       ! call MAPL_TimerOn(MAPL, "TOTAL")
@@ -2807,7 +2795,6 @@ contains
       !   call RunAddIncs(gc, import, export, clock, rc)
       !endif
 
-      call logger%info("Run:: ...complete")
       _RETURN(_SUCCESS)
 
    contains
