705a3ac69a871a972341dcd6e0cccf48850eaea2
Tom Clune <thomas.l.clune@nasa.gov>
2026-05-01 20:43:48 -0400

    AppGridCreate: add MAPL_MemUtilsWrite to MAPL2 only: in AppGridCreateF

--- AppGridCreate.F90
+++ AppGridCreate.F90
@@ -129,7 +129,7 @@ function AppGridCreateF(IM_WORLD, JM_WORLD, LM, NX, NY, rc) result(esmfgrid)
 
    use ESMF
    use MAPL
-   use MAPL2, only: MAPL_AllocNodeArray, MAPL_DeAllocNodeArray
+   use MAPL2, only: MAPL_AllocNodeArray, MAPL_DeAllocNodeArray, MAPL_MemUtilsWrite
    use MAPL_Constants, only: pi => MAPL_PI_R8
 
    use fv_arrays_mod,     only: REAL4, REAL8, R_GRID
