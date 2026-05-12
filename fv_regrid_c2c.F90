58b2802d5ea2459e9f15db1d61453f4d7705c9ff
Tom Clune <thomas.l.clune@nasa.gov>
2026-05-12 10:06:22 -0400

    Use mapl_GridGetGlobalCellCountPerDim via top-level MAPL module

--- fv_regrid_c2c.F90
+++ fv_regrid_c2c.F90
@@ -20,7 +20,7 @@ module fv_regrid_c2c
    use mapl3g_CubedSphereGeomSpec
    use MAPL_Constants,           only: MAPL_PI_R8, MAPL_OMEGA, MAPL_GRAV, MAPL_KAPPA, &
                                        MAPL_RGAS, MAPL_RVAP, MAPL_CP, MAPL_PSDRY
-   use mapl3g_Geom_API,           only: mapl_GridGetGlobalCellCountPerDim
+   use MAPL,                      only: mapl_GridGetGlobalCellCountPerDim
    use MAPL_CommsMod,            only: ArrayScatter
    use FileIOSharedMod,          only: ArrDescr, ArrDescrInit, ArrDescrSet
    use NCIOMod,                  only: MAPL_VarRead, MAPL_NCIOGetFileType, &
