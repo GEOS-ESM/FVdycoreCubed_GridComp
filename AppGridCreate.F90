002533c795a94864b61b30af1e7decfe25319f5f
Tom Clune <thomas.l.clune@nasa.gov>
2026-05-12 08:54:39 -0400

    Fix ifort resolution of pi in AppGridCreate.F90 (#373)
    Use rename syntax 'use MAPL, pi => MAPL_PI_R8' rather than a separate
    'use MAPL_Constants, only: pi => MAPL_PI_R8'. The latter caused ifort
    to fail after MAPL PR #4859 changed the transitive export chain of
    MAPL2/MAPLBase_Mod.

--- AppGridCreate.F90
+++ AppGridCreate.F90
@@ -4,7 +4,7 @@ subroutine AppCSEdgeCreateF(IM_WORLD, LonEdge,LatEdge, LonCenter, LatCenter, rc)
 #include "MAPL_Generic.h"
 
    use ESMF
-   use MAPL, only: pi => MAPL_PI_R8
+   use MAPL, pi => MAPL_PI_R8
    use MAPL2, only: MAPL_AllocNodeArray, MAPL_DeAllocNodeArray, &
         MAPL_MemUtilsWrite
    use fv_arrays_mod,     only: REAL4, REAL8, R_GRID
@@ -127,7 +127,7 @@ function AppGridCreateF(IM_WORLD, JM_WORLD, LM, NX, NY, rc) result(esmfgrid)
 #define DEALLOCGLOB_(A) if(associated(A))then;A=0;if(MAPL_ShmInitialized)then; call MAPL_DeAllocNodeArray(A,rc=STATUS);else; deallocate(A,stat=STATUS);endif;VERIFY_(STATUS);NULLIFY(A);endif
 
    use ESMF
-   use MAPL, only: pi => MAPL_PI_R8
+   use MAPL, pi => MAPL_PI_R8
    use MAPL2, only: MAPL_AllocNodeArray, MAPL_DeAllocNodeArray, &
         MAPL_MemUtilsWrite
    use mapl3g_GridGet, only: mapl_GridGet => GridGet
