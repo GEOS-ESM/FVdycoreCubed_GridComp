e28eed630e0756d7b9510a91575cfec8c06f598e
Florian Deconinck <deconinck.florian@gmail.com>
2026-06-19 15:59:27 -0400

    Close `FV_FIRST_RUN` Timer

--- FV_StateMod.F90
+++ FV_StateMod.F90
@@ -1406,6 +1406,7 @@ subroutine FV_Run (STATE, EXPORT, CLOCK, GC, RC)
 #endif
 
    endif
+   call MAPL_TimerOff(MAPL,"--FV_FIRST_RUN")
    call MAPL_TimerOff(MAPL,"--MASS_FIX")
 
    select case ( FV_Atm(1)%flagstruct%nwat )
