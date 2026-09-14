baf665b371d117349d1e971c7accbc9db67a14e5
Florian Deconinck <deconinck.florian@gmail.com>
2026-09-14 07:36:52 -0400

    Revert "Config: swap fv/py to control boolean behavior through casting"
    This reverts commit 9280abb09d68a7d22269c954b91d7b4a70c22cca.

--- python/interface/fv_flags.py
+++ python/interface/fv_flags.py
@@ -1,4 +1,3 @@
-import warnings
 import dataclasses
 from typing import Union
 from pyfv3._config import (
@@ -174,16 +173,14 @@ def _generic_config_bridge(
     ],
     fv_config: FVFlags,
 ):
-    keys = list(filter(lambda k: not k.startswith("__"), dir(type(fv_config))))
+    keys = list(filter(lambda k: not k.startswith("__"), dir(type(py_config))))
     for k in keys:
-        if hasattr(py_config, k):
+        if hasattr(fv_config, k):
             v = getattr(fv_config, k)
             if isinstance(v, bool):
                 setattr(py_config, k, bool(v))
             else:
                 setattr(py_config, k, v)
-        else:
-            warnings.warn(f"FV Config {k} does not exist in pyFV configuration")
 
 
 def FVFlags_to_DycoreConfig(
@@ -196,4 +193,7 @@ def FVFlags_to_DycoreConfig(
         )
 
     _generic_config_bridge(py_config, fv_config)
-    py_config.layout = (fv_config.layout_x, fv_config.layout_y)
+    py_config.layout = (
+        getattr(fv_config, "layout_x"),
+        getattr(fv_config, "layout_y"),
+    )
