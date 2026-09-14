f9faf8e0a336b7483979a8ce539796c3ab6cd419
Florian Deconinck <deconinck.florian@gmail.com>
2026-09-14 07:18:18 -0400

    Revert previous and cast bool

--- python/interface/fv_flags.py
+++ python/interface/fv_flags.py
@@ -42,40 +42,40 @@ class FVFlags:
     damp_k_k2: float
     n_zs_filter: int
     nord_zs_filter: int
-    full_zs_filter: bool | int
-    RF_fast: bool | int
-    Beljaars_TOFD: bool | int
-    consv_am: bool | int
-    do_sat_adj: bool | int
-    do_f3d: bool | int
-    no_dycore: bool | int
-    convert_ke: bool | int
-    do_vort_damp: bool | int
-    use_old_omega: bool | int
+    full_zs_filter: bool
+    RF_fast: bool
+    Beljaars_TOFD: bool
+    consv_am: bool
+    do_sat_adj: bool
+    do_f3d: bool
+    no_dycore: bool
+    convert_ke: bool
+    do_vort_damp: bool
+    use_old_omega: bool
     beta: float
     n_zfilter: int
     n_sponge: int
     d_ext: float
     nwat: int
-    warm_start: bool | int
-    inline_q: bool | int
-    adiabatic: bool | int
+    warm_start: bool
+    inline_q: bool
+    adiabatic: bool
     shift_fac: float
-    do_schmidt: bool | int
+    do_schmidt: bool
     stretch_fac: float
     target_lat: float
     target_lon: float
-    reset_eta: bool | int
+    reset_eta: bool
     p_fac: float
     a_imp: float
     dz_min: float
     n_split: int
     m_split: int
     k_split: int
-    use_logp: bool | int
+    use_logp: bool
     q_split: int
     print_freq: int
-    write_3d_diags: bool | int
+    write_3d_diags: bool
     npx: int
     npy: int
     npz: int
@@ -88,7 +88,7 @@ class FVFlags:
     nf_omega: int
     fv_sg_adj: int
     na_init: int
-    nudge_dz: bool | int
+    nudge_dz: bool
     p_ref: float
     dry_mass: float
     nt_prog: int
@@ -100,52 +100,52 @@ class FVFlags:
     consv_te: float
     tau: float
     rf_cutoff: float
-    filter_phys: bool | int
-    dwind_2d: bool | int
-    breed_vortex_inline: bool | int
-    range_warn: bool | int
-    fill: bool | int
-    fill_dp: bool | int
-    fill_wz: bool | int
-    check_negative: bool | int
-    non_ortho: bool | int
-    moist_phys: bool | int
-    do_Held_Suarez: bool | int
-    do_reed_physics: bool | int
-    reed_cond_only: bool | int
-    reproduce_sum: bool | int
-    adjust_dry_mass: bool | int
-    fv_debug: bool | int
-    srf_init: bool | int
-    mountain: bool | int
-    old_divg_damp: bool | int
+    filter_phys: bool
+    dwind_2d: bool
+    breed_vortex_inline: bool
+    range_warn: bool
+    fill: bool
+    fill_dp: bool
+    fill_wz: bool
+    check_negative: bool
+    non_ortho: bool
+    moist_phys: bool
+    do_Held_Suarez: bool
+    do_reed_physics: bool
+    reed_cond_only: bool
+    reproduce_sum: bool
+    adjust_dry_mass: bool
+    fv_debug: bool
+    srf_init: bool
+    mountain: bool
+    old_divg_damp: bool
     remap_option: int
     gmao_remap: int
-    z_tracer: bool | int
-    fv_land: bool | int
-    nudge: bool | int
-    nudge_ic: bool | int
-    ncep_ic: bool | int
-    nggps_ic: bool | int
-    ecmwf_ic: bool | int
-    gfs_phil: bool | int
-    agrid_vel_rst: bool | int
-    use_new_ncep: bool | int
-    use_ncep_phy: bool | int
-    fv_diag_ic: bool | int
-    external_ic: bool | int
-    external_eta: bool | int
-    read_increment: bool | int
-    do_skeb: bool | int
+    z_tracer: bool
+    fv_land: bool
+    nudge: bool
+    nudge_ic: bool
+    ncep_ic: bool
+    nggps_ic: bool
+    ecmwf_ic: bool
+    gfs_phil: bool
+    agrid_vel_rst: bool
+    use_new_ncep: bool
+    use_ncep_phy: bool
+    fv_diag_ic: bool
+    external_ic: bool
+    external_eta: bool
+    read_increment: bool
+    do_skeb: bool
     skeb_npass: int
-    hydrostatic: bool | int
-    phys_hydrostatic: bool | int
-    use_hydro_pressure: bool | int
-    do_uni_zfull: bool | int
-    hybrid_z: bool | int
-    Make_NH: bool | int
-    make_hybrid_z: bool | int
-    nudge_qv: bool | int
+    hydrostatic: bool
+    phys_hydrostatic: bool
+    use_hydro_pressure: bool
+    do_uni_zfull: bool
+    hybrid_z: bool
+    Make_NH: bool
+    make_hybrid_z: bool
+    nudge_qv: bool
     add_noise: float
     a2b_ord: int
     c2l_ord: int
@@ -153,8 +153,8 @@ class FVFlags:
     dy_const: float
     deglat: float
     deglon_start: float
-    adj_mass_vmr: bool | int
-    compute_coords_locally: bool | int
+    adj_mass_vmr: bool
+    compute_coords_locally: bool
     # Grid
     layout_x: int
     layout_y: int
@@ -176,7 +176,11 @@ def _generic_config_bridge(
     keys = list(filter(lambda k: not k.startswith("__"), dir(type(py_config))))
     for k in keys:
         if hasattr(fv_config, k):
-            setattr(py_config, k, getattr(fv_config, k))
+            v = getattr(fv_config, k)
+            if isinstance(v, bool):
+                setattr(py_config, k, bool(v))
+            else:
+                setattr(py_config, k, v)
 
 
 def FVFlags_to_DycoreConfig(
